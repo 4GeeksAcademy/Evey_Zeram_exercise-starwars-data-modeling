c4e74d0c3518f525898b25712e3b028dc6d9bcdc
Evelyn <130219194+EveyZeram@users.noreply.github.com>
2024-01-11 17:24:43 +0000

    EveyZeran SW Done

--- src/models.py
+++ src/models.py
@@ -8,23 +8,18 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-# OPCION 2: Ejercicio con tabla de asociaciones
-
-# crear tabla de asociciones para todas las relaciones
-
+#Usando modelo con tabla de relaciones:
 association_table = Table('association', Base.metadata,
-    Column('User', String, ForeignKey('user.id'), primary_key=True),
+    Column('Users', String, ForeignKey('users.id'), primary_key=True),
     Column('Movies', String, ForeignKey('movies.id'), primary_key=True),
     Column('Planets', String, ForeignKey('planets.id'), primary_key=True),
-    Column('People', String, ForeignKey('people.id'), primary_key=True),
+    Column('Character', String, ForeignKey('characters.id'), primary_key=True),
 )
 
-# Crear tabla: user, planets, movies y people.
+# Crear tabla: users, planets, movies y character.
+class Users(Base):
+    __tablename__ = 'users'
 
-class User(Base):
-    __tablename__ = 'user'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
     lastname = Column(String())
@@ -32,59 +27,71 @@ class User(Base):
     email = Column(String(), nullable=False)
     password = Column(String(10), nullable=False)
     subscription_date = Column(Date)
-    # Añadimos los "hijos" asociando a la tabla los id
-    # El atributo secondary se utiliza para especificar la tabla de asociación que conecta las dos tablas principales en una relación muchos a muchos
-    # Back ref relaciona planets con User.
-    children = relationship('Planets', secondary=association_table, backref='User')
-    children = relationship('Movies', secondary=association_table, backref='User')
-    children = relationship('People', secondary=association_table, backref='User')
+    children = relationship('Planets', secondary=association_table, backref='Users')
+    children = relationship('Movies', secondary=association_table, backref='Users')
+    children = relationship('Characters', secondary=association_table, backref='Users')
+
+class Profiles(Base):
+    __tablename__ = 'profiles'
+    id = Column(Integer, primary_key=True)
+    lastname = Column(String(20))
+    firtname = Column(String(20))
+    nickname = Column(String(6))
+    imgurl = Column(String())
+    users_id = Column(Integer, ForeignKey('users.id'), unique=True) #Define un user a un perfil únicamente
+    users = relationship(Users)
+
 
 class Planets(Base):
     __tablename__ = 'planets'
     id = Column(Integer, primary_key = True)
     name = Column(String, nullable=False)
-    diameter = Column(Integer, nullable=False)
-    rotation_period = Column(Integer, nullable=False)
-    orbital_period = Column(Integer, nullable=False)
-    gravity = Column(Integer, nullable=False)        
-    population = Column(Integer, nullable=False)
-    climate = Column(String, nullable=False)
-    terrain = Column(String, nullable=False)
-    surface_water = Column(Integer, nullable=False)
+    diameter = Column(Integer)
+    rotation_period = Column(Integer)
+    orbital_period = Column(Integer)
+    gravity = Column(Integer)        
+    population = Column(Integer)
+    climate = Column(String)
+    terrain = Column(String)
+    surface_water = Column(Integer)
     url = Column(String, nullable=False) 
     movies = Column(String, nullable=False)
-    # Padre = User
-    parent_id = Column(Integer, ForeignKey('User.id'))            
+    parent_id = Column(Integer, ForeignKey('Users.id'))            
  
 class Movies(Base):
     __tablename__ = 'movies'
     id = Column(Integer, primary_key = True)
     title = Column(String, nullable=False)
     year  = Column(Integer, nullable=False)
-    parent_id = Column(Integer, ForeignKey('User.id'))  
+    parent_id = Column(Integer, ForeignKey('Users.id'))  
 
-class People(Base):
-    __tablename__ = 'people'
+class Characters(Base):
+    __tablename__ = 'characters'
     id = Column(Integer, primary_key = True)
     name = Column(String, nullable=False)
     gender = Column(String, nullable=False)
-    height = Column(Integer, nullable=False)
-    mass = Column(Integer, nullable=False)
-    hair_color = Column(String, nullable=False)
-    skin_color = Column(String, nullable=False)
-    parent_id = Column(Integer, ForeignKey('User.id'))  
+    height = Column(Integer)
+    mass = Column(Integer)
+    hair_color = Column(String)
+    skin_color = Column(String)
+    parent_id = Column(Integer, ForeignKey('Users.id'))  
+
+#Creamos las tablas de favoritos. Opción1:
+class FavoriteCharacters(Base):
+    __tablename__ = "favorite_characters"
+    id = Column(Integer, primary_key=True)
+    users_id = Column(Integer, ForeignKey('users.id'))
+    characters_id = Column(Integer, ForeignKey('characters.id'))
+    users = relationship(Users) 
+    characters = relationship (Characters)
 
 class FavoritePlanets(Base):
-    __tablename__ = 'favorite_planets'
+    __tablename__ = "favorite_planets"
     id = Column(Integer, primary_key=True)
-    # Hay que definir la columna = clave foránea
-    user_id = Column(Integer, ForeignKey(User.id))
-    # Definir la relación de esa FK
-    user = relationship(User) # Aquí se debe definir que FK utiliza esta relación
-    planet_id = Column(Integer, ForeignKey(Planets.id))
-    planet = relationship (Planets)
-
-        ## Draw from SQLAlchemy base
+    users_id = Column(Integer, ForeignKey('users.id'))
+    planets_id = Column(Integer, ForeignKey('planets.id'))
+    users = relationship(Users) 
+    planets = relationship (Planets)
 
 
 
